7f35c9d608a61fef2e17b38099dd7431be1efaa6
Ousman <usman.traid@gmail.com>
2023-05-02 09:59:08 -0400

    requirement fixed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,13 @@
 1-concurrent_coroutines
 """
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """runs wait_random n times"""
     all_tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     sorted_tasks = [await task for task in asyncio.as_completed(all_tasks)]
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,12 +3,13 @@
 4-tasks
 """
 import asyncio
+from typing import List
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """runs task_wait_random n times"""
     all_tasks = [task_wait_random(max_delay) for i in range(n)]
     sorted_tasks = [await task for task in asyncio.as_completed(all_tasks)]
